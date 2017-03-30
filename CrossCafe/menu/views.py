7439782b59896ed0b634dd8daa385b7b9fee1594
Saurabh Jain <saurabh.j@students.iiit.ac.in>
2017-03-30 14:27:57 +0530

    standardizing according to PEP8

--- CrossCafe/menu/views.py
+++ CrossCafe/menu/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from menu.models import FoodItem
 
-def showMenu(request):
-	if(request.GET.get('restaurant_id')):
-		restaurant_id = request.GET.get('restaurant_id')
-		menu = FoodItem.objects.filter(restaurant_id= restaurant_id, status = True)
-	else:
-		home_page_url = '/'
-		return HttpResponseRedirect(home_page_url)
-	return render(request, 'menu/show_menu.html', {'menu': menu})
 
+def showMenu(request):
+    if(request.GET.get('restaurant_id')):
+        restaurant_id = request.GET.get('restaurant_id')
+        menu = FoodItem.objects.filter(restaurant_id=restaurant_id, status=True)
+    else:
+        home_page_url = '/'
+        return HttpResponseRedirect(home_page_url)
+    return render(request, 'menu/show_menu.html', {'menu': menu})
--- CrossCafe/restaurant/views.py
+++ CrossCafe/restaurant/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from restaurant.models import Restaurant
 from django.core import serializers
 
-def index(request):
-	all_cities = Restaurant.objects.values('city').distinct()
-	city_areas = Restaurant.objects.filter(city = all_cities[0]['city']).only('area').distinct()
-	city_areas = serializers.serialize('json', list(city_areas), fields=('area'))
-	return render(request, 'index.html', {'all_cities': all_cities, 'city_areas': city_areas})
 
-# Handle all cases: If not city, if not area, if not get request
+# if-else vs. try-except
+def index(request):
+    all_cities = Restaurant.objects.values('city').distinct()
+    city_areas = Restaurant.objects.filter(city=all_cities[0]['city']).only('area').distinct()
+    if(city_areas):
+        city_areas = serializers.serialize('json', list(city_areas), fields=('area'))
+        return render(request, 'index.html', {'all_cities': all_cities, 'city_areas': city_areas})
+    else:
+        return HttpResponse('<h1>Restaurant not available in your city</h1>')
+
+
+# Handle different case types (uppercase, lowercase, etc)
 def getAreas(request):
-	city = request.GET.get('city')
-	city_areas = Restaurant.objects.filter(city = city).only('area').distinct()
-	city_areas = serializers.serialize('json', list(city_areas), fields=('area'))
-	return HttpResponse(city_areas)
+    if(request.GET.get('city')):
+        city = request.GET.get('city')
+        city_areas = Restaurant.objects.filter(city=city).only('area').distinct()
+        if(city_areas):
+            city_areas = serializers.serialize('json', list(city_areas), fields=('area'))
+            return HttpResponse(city_areas)
+        else:
+            return HttpResponse('<h1>Restaurant not available in your city</h1>')
+    else:
+        home_page_url = '/'
+        return HttpResponseRedirect(home_page_url)
