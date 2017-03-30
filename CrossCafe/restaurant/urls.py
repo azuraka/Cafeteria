5bb790bb9e00ac2d228f85d6de907609c2726e8c
Saurabh Jain <saurabh.j@students.iiit.ac.in>
2017-03-30 13:19:14 +0530

    Dynamic city and area loading via AJAX

--- CrossCafe/restaurant/urls.py
+++ CrossCafe/restaurant/urls.py
@@ -2,8 +2,10 @@
 """
 from django.conf.urls import url
 from restaurant import search_restaurant
+from restaurant import views
 
 urlpatterns = [
     url(r'^gps_search', search_restaurant.gpsSearch, name='gpsSearch'),
     url(r'^dropdown_search', search_restaurant.dropdownSearch, name='dropdownSearch'),
+    url(r'^get_areas', views.getAreas, name='getAreas'),
 ]
--- CrossCafe/restaurant/views.py
+++ CrossCafe/restaurant/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from restaurant.models import Restaurant
+from django.core import serializers
 
 def index(request):
-	return render(request, 'index.html')
+	all_cities = Restaurant.objects.values('city').distinct()
+	city_areas = Restaurant.objects.filter(city = all_cities[0]['city']).only('area').distinct()
+	city_areas = serializers.serialize('json', list(city_areas), fields=('area'))
+	return render(request, 'index.html', {'all_cities': all_cities, 'city_areas': city_areas})
+
+# Handle all cases: If not city, if not area, if not get request
+def getAreas(request):
+	city = request.GET.get('city')
+	city_areas = Restaurant.objects.filter(city = city).only('area').distinct()
+	city_areas = serializers.serialize('json', list(city_areas), fields=('area'))
+	return HttpResponse(city_areas)
